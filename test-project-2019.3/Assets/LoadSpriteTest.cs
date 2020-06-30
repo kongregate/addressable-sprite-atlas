450a057aa66a07595fa426d21bc3d8c9b9964b44
David LeGare <dlegare.1001@gmail.com>
2020-06-30 11:06:06 -0500

    Setup failing example

--- test-project-2019.3/Assets/LoadSpriteTest.cs
+++ test-project-2019.3/Assets/LoadSpriteTest.cs
@@ -8,16 +8,24 @@ public class LoadSpriteTest : MonoBehaviour
 {
     IEnumerator Start()
     {
-        var handle = Addressables.LoadAssetAsync<Sprite>("happy_non_atlas");
+        yield return TryLoadSprite("happy_non_atlas");
+        yield return TryLoadSprite("sad_non_atlas");
+    }
+
+    private IEnumerator TryLoadSprite(string address)
+    {
+        Debug.Log($"Attempting to load sprite @ {address}");
+
+        var handle = Addressables.LoadAssetAsync<Sprite>(address);
         yield return handle;
 
         if (AsyncOperationStatus.Succeeded == handle.Status)
         {
-            Debug.Log($"Successfully loaded sprite: {handle.Result}");
+            Debug.Log($"Successfully loaded sprite @ {address}: {handle.Result}");
         }
         else
         {
-            Debug.LogError($"Failed to load sprite: {handle.OperationException}");
+            Debug.LogError($"Failed to load sprite @ {address}: {handle.OperationException}");
         }
     }
 }
